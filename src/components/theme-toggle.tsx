307c5cd1f970345b0afb108a2881a7701228d382
ahmad9059 <ahmadhassan9059@gmail.com>
2025-11-09 01:31:08 +0500

    theme switcher fixed/errors

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -32,10 +32,10 @@ export function ThemeToggle() {
           <Moon className="mr-2 h-4 w-4" />
           <span>Dark</span>
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        {/* <DropdownMenuItem onClick={() => setTheme("system")}>
           <span className="mr-2">💻</span>
           <span>System</span>
-        </DropdownMenuItem>
+        </DropdownMenuItem> */}
       </DropdownMenuContent>
     </DropdownMenu>
   );
